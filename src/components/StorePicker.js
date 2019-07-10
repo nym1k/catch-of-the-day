b34d1fc003fc68b9bf333e9a8c0d1bc6ba0853e6
Ross King <sleeping.stars@gmail.com>
2019-07-10 07:57:51 +0100

    Added form to add fish, and set state in app component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = e => {
+    // 1. Stop form from submitting
+    e.preventDefault()
+    // 2. Get the text from that input
+    const storeName = this.myInput.current.value
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`)
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     )
