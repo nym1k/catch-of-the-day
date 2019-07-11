d305669e253e5d09f227ddba7fc15df0ce50b0dd
Ross King <sleeping.stars@gmail.com>
2019-07-11 08:04:56 +0100

    Added sample data, displayed in menu, add to order functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
+import Fish from './Fish'
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   }
-  addFish = (fish) => {
+  addFish = fish => {
     // 1. Take a copy of the existing state (Dont edit directly, thats a mutation (reaching directly in to an object))
     const fishes = { ...this.state.fishes }
     // 2. Add our new fish to that fishes variable
@@ -16,14 +18,37 @@ class App extends React.Component {
     // 3. Set the new fishes object to state
     this.setState({ fishes })
   }
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  }
+
+  addToOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order }
+    // 2. Either add to the order, or update the quantity in our order
+    order[key] = order[key] + 1 || 1
+    // 3. Call setState to update our state object
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            { Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            )) }
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }
